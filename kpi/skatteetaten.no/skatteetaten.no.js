54e03414a4df8b4e852a9311f1e59919ab366251
daphneleebeek <daphneleebeek@gmail.com>
2016-07-03 23:20:07 +0200

    fixed menu

--- kpi/skatteetaten.no/skatteetaten.no.js
+++ kpi/skatteetaten.no/skatteetaten.no.js
@@ -2,31 +2,33 @@
  * Created by Administrator on 30.06.2016.
  */
 
+//Current URL
+var current_page_URL = location.href;
 
+//Current path in hierarchy
+var current_sideMenu_URL  = current_page_URL.split("skatteetaten.no/")[1];
+var current_subSideMenu_URL  = current_page_URL.split("person/")[1];
+
+//Side menu link & sub side menu link Selectors
+var menulink = $('.sidemenu-link');
+var submenulink =  $('.sub-sidemenu-link');
 
 $(document).ready(function () {
 
-
-
-   /* $(function changeActive(){
-        var current_page_URL = location.href;
-        $(".sidemenu-link").each(function() {
-            if ($(this).attr("href") !== "#") {
-                var target_URL = $(this).prop("href");
-                if (target_URL == current_page_URL) {
-                    $('.sidemenu-link').parents('li, ul').removeClass('active');
-                    $(this).parent('li').addClass('active');
-                    showSideSubMenu();
-                }
-            }
-        });
-    });*/
-
-    function showSideSubMenu(){
-        if($("#person").hasClass('active')){
-            $("#person ul").css( "display", "block" );
-        };
+    //Changes active class on buttons in side menu and sub side menu
+    function changeActive() {
+        menulink.parents('li, ul').removeClass('active');
+        submenulink.parents('li, ul').removeClass('active');
+        menulink.each(function () {
+            if ($(this).attr("href") !== "#" && $(this).attr("href").split("skatteetaten.no/")[1].substr(0,4) === current_sideMenu_URL.substr(0,4)) {
+                $(this).parent('li').addClass('active');
+            }});
+        submenulink.each(function () {
+            if ($(this).attr("href") !== "#" && $(this).attr("href").split("person/")[1].substr(0,4) === current_subSideMenu_URL.substr(0,4)) {
+                $(this).parent('li').addClass('active');
+            }});
     };
 
+    window.onload = changeActive();
 
 });
