a5fa9ca978a5d05268a41b20c00afc5ec6276eb3
Daphne Leebeek <daphneleebeek@gmail.com>
2016-07-06 15:25:37 +0200

    created a header with numbers

--- kpi/kpi.js
+++ kpi/kpi.js
@@ -6,18 +6,23 @@ var current_page_URL = location.href;
 var current_top_URL  = current_page_URL.split("kpi/")[1];
 
 
+
+
     $(document).ready(function () {
+
         function changeActive() {
             $(".menu-link-top").each(function () {
                 if ($(this).attr("href") !== "#" && $(this).attr("href").split("kpi/")[1].substr(0,4) === current_top_URL.substr(0,4)) {
                     $('.menu-link-top').parents('li, ul').removeClass('active');
                     $(this).parent('li').addClass('active');
+                    $('div.name-date-header h1.nameOfSite').text($(this).text());
+
                 }
             });
         };
         
         window.onload = changeActive();
-        
+
 
     });
 
--- kpi/skatteetaten.no/skatteetaten.no.js
+++ kpi/skatteetaten.no/skatteetaten.no.js
@@ -6,29 +6,50 @@
 var current_page_URL = location.href;
 
 //Current path in hierarchy
-var current_sideMenu_URL  = current_page_URL.split("skatteetaten.no/")[1];
-var current_subSideMenu_URL  = current_page_URL.split("person/")[1];
+var current_sideMenu_path  = current_page_URL.split("skatteetaten.no/")[1];
+var current_subSideMenu_path  = current_page_URL.split("person/")[1];
 
-//Side menu link & sub side menu link Selectors
+//Side menu link & sub side menu link/Selectors of <a>
 var menulink = $('.sidemenu-link');
 var submenulink =  $('.sub-sidemenu-link');
 
+
 $(document).ready(function () {
+
 
     //Changes active class on buttons in side menu and sub side menu
     function changeActive() {
         menulink.parents('li, ul').removeClass('active');
         submenulink.parents('li, ul').removeClass('active');
         menulink.each(function () {
-            if ($(this).attr("href") !== "#" && $(this).attr("href").split("skatteetaten.no/")[1].substr(0,4) === current_sideMenu_URL.substr(0,4)) {
+            if ($(this).attr("href") !== "#" && $(this).attr("href").split("skatteetaten.no/")[1].substr(0, 4) === current_sideMenu_path.substr(0, 4)) {
                 $(this).parent('li').addClass('active');
-            }});
+                $('div.subHeaderDate h2.subHeader').text($(this).text());
+            }
+        });
         submenulink.each(function () {
-            if ($(this).attr("href") !== "#" && $(this).attr("href").split("person/")[1].substr(0,4) === current_subSideMenu_URL.substr(0,4)) {
+            if ($(this).attr("href") !== "#" && $(this).attr("href").split("person/")[1].substr(0, 4) === current_subSideMenu_path.substr(0, 4)) {
                 $(this).parent('li').addClass('active');
-            }});
+                $(this).children('.sidebar-dot').css("background-color", "#6f2c3f");
+            }
+        });
     };
-
     window.onload = changeActive();
 
+
+    //DatePicker
+    $(function () {
+        $("#datepicker").datepicker({
+            numberOfMonths: 2
+        });
+    });
+
+    //Drag down of main box
+    $(function() {
+        $( "#accordion" ).accordion();
+    });
+    
+
+
+    
 });
