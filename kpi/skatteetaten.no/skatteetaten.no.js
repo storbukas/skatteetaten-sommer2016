4b8d2af3b6088a555595fa47db7360e66c68e413
Daphne Leebeek <daphneleebeek@gmail.com>
2016-07-13 15:01:42 +0200

    added status

--- kpi/skatteetaten.no/skatteetaten.no.js
+++ kpi/skatteetaten.no/skatteetaten.no.js
@@ -50,8 +50,6 @@ $(document).ready(function () {
 
     
 
-    
-    
     //Read JSON object
     $.ajax({
         type: 'GET',
@@ -60,7 +58,10 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
 
-            
+
+            var statusName = ["Bra", "Advarsel", "Kritisk"];
+            var statusColor = ["green", "#ffaa00", "red"];
+
             /*create content of accordion*/
             $.each(data.top_pages, function(index, element){
                 
@@ -114,9 +115,9 @@ $(document).ready(function () {
                 actual.innerHTML = 'Faktiske';
                 goal.innerHTML = 'Mål';
                 trend.innerHTML = 'Trend';
-                goalDeviation.innerHTML = 'Mål %';
+                goalDeviation.innerHTML = 'Status';
 
-                //Append labels
+                //Append labels to table head (thead)
                 trHead.appendChild(empty_th);
                 trHead.appendChild(actual);
                 trHead.appendChild(goal);
@@ -133,17 +134,22 @@ $(document).ready(function () {
                 var tdBR_actual = document.createElement('td');
                 var tdBR_goal = document.createElement('td');
                 var tdBR_trend = document.createElement('td'); /*************************LAGE TREND****/
-                var tdBR_goalDeviation = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
+                var tdBR_status = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
                 var bounceRate = Math.round(element.bounce_rate* 10) / 10;
+                var br_calculateStatus = calculateStatusBounceRate(bounceRate);
                 thBounceRate.setAttribute("scope", "row");
+
                 thBounceRate.innerHTML = "Fluktfrekvens";
                 tdBR_actual.innerHTML = bounceRate + '%';
-                tdBR_goal.innerHTML = "30%"; /*********Set manually********/
+                tdBR_goal.innerHTML = "< 25%"; /*********Set manually********/
+                tdBR_status.innerHTML = statusName[br_calculateStatus];
+                tdBR_status.style.cssText = 'color: ' + statusColor[br_calculateStatus];
+
                 trBounceRate.appendChild(thBounceRate);
                 trBounceRate.appendChild(tdBR_actual);
                 trBounceRate.appendChild(tdBR_goal);
                 trBounceRate.appendChild(tdBR_trend);
-                trBounceRate.appendChild(tdBR_goalDeviation);
+                trBounceRate.appendChild(tdBR_status);
 
                 //Pageviews VS unique row
                 var trPageviews = document.createElement('tr');
@@ -151,17 +157,22 @@ $(document).ready(function () {
                 var tdPV_actual = document.createElement('td');
                 var tdPV_goal = document.createElement('td');
                 var tdPV_trend = document.createElement('td'); /*************************LAGE TREND****/
-                var tdPV_goalDeviation = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
-                var pageviews = Math.round((element.pageviews/element.unique_pageviews) *10)/ 10;
+                var tdPV_status = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
+                var pageviews =  Math.round((element.pageviews/element.unique_pageviews) *10)/ 10;
+                var pv_calculateStatus = calculateStatusPageviews(pageviews);
                 thPageviews.setAttribute("scope", "row");
+
                 thPageviews.innerHTML = "Sidevisninger ÷ Unike";
                 tdPV_actual.innerHTML = pageviews;
-                tdPV_goal.innerHTML = "1"; /*********Set manually********/
+                tdPV_goal.innerHTML = "< 1.3"; /*********Set manually********/
+                tdPV_status.innerHTML = statusName[pv_calculateStatus];
+                tdPV_status.style.cssText = 'color: ' + statusColor[pv_calculateStatus];
+
                 trPageviews.appendChild(thPageviews);
                 trPageviews.appendChild(tdPV_actual);
                 trPageviews.appendChild(tdPV_goal);
                 trPageviews.appendChild(tdPV_trend);
-                trPageviews.appendChild(tdPV_goalDeviation);
+                trPageviews.appendChild(tdPV_status);
 
                 //Average time spent on page
                 var trAverageTime = document.createElement('tr');
@@ -169,116 +180,39 @@ $(document).ready(function () {
                 var tdAT_actual = document.createElement('td');
                 var tdAT_goal = document.createElement('td');
                 var tdAT_trend = document.createElement('td'); /*************************LAGE TREND****/
-                var tdAT_goalDeviation = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
+                var tdAT_status = document.createElement('td'); /*********************LAGE MÅL AVVIK*/
                 var avg_time_on_page = moment.duration(Math.round(element.avg_time_on_page), "seconds").format("mm:ss", { trim: false });
                 thAverageTime.setAttribute("scope", "row");
                 thAverageTime.innerHTML = "Gjennomsnittstid";
                 tdAT_actual.innerHTML = avg_time_on_page;
 
-
-
-                // jquery.xdomainajax.js  ------ from padolsey
-
-                jQuery.ajax = (function(_ajax){
-
-                    var protocol = location.protocol,
-                        hostname = location.hostname,
-                        exRegex = RegExp(protocol + '//' + hostname),
-                        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
-                        query = 'select * from html where url="{URL}" and xpath="*"';
-
-                    function isExternal(url) {
-                        return !exRegex.test(url) && /:\/\//.test(url);
-                    }
-
-                    return function(o) {
-
-                        var url = o.url;
-
-                        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
-
-                            // Manipulate options so that JSONP-x request is made to YQL
-
-                            o.url = YQL;
-                            o.dataType = 'json';
-
-                            o.data = {
-                                q: query.replace(
-                                    '{URL}',
-                                    url + (o.data ?
-                                    (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
-                                        : '')
-                                ),
-                                format: 'xml'
-                            };
-
-                            // Since it's a JSONP request
-                            // complete === success
-                            if (!o.success && o.complete) {
-                                o.success = o.complete;
-                                delete o.complete;
-                            }
-
-                            o.success = (function(_success){
-                                return function(data) {
-
-                                    if (_success) {
-                                        // Fake XHR callback.
-                                        _success.call(this, {
-                                            responseText: data.results[0]
-                                            // YQL screws with <script>s
-                                            // Get rid of them
-                                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
-                                        }, 'success');
-                                    }
-
-                                };
-                            })(o.success);
-
-                        }
-
-                        return _ajax.apply(this, arguments);
-
-                    };
-
-                })(jQuery.ajax);
-
-
-
+                //get URL using ajax
                 $.ajax({
                     url: element.url,
                     type: 'GET',
-                    success: function(response) {
+                    async: false,
+                    success: function (response) {
+                        //removes all blank spaces and tags
                         var text = response.responseText.replace(/[\n\r]/g, '').replace(/&#xd;/g, '').replace(/\//g, '').replace(/<.*?>/g, '').replace(/  +/g, ' ');
-                        console.log(text);
-
-
+                        //count the words
                         var wordsCount = text.split(' ').length;
-                        console.log(wordsCount);
-
-
-
-
+                        tdAT_goal.innerHTML = (moment.duration((wordsCount/500), "minutes").format("mm:ss", {trim: false}))
+                            + " - "
+                            + moment.duration((wordsCount/150), "minutes").format("mm:ss", {trim: false}); /*********Set manually*/
+                        var at_calculateStatus = calculateStatusAverageTime(element.avg_time_on_page, (wordsCount/500)*60, (wordsCount/150)*60);
+                        tdAT_status.innerHTML = statusName[at_calculateStatus];
+                        tdAT_status.style.cssText = 'color: ' + statusColor[at_calculateStatus];
                     }
                 });
 
 
-                tdAT_goal.innerHTML = ""; /*********Set manually*/
+
+
                 trAverageTime.appendChild(thAverageTime);
                 trAverageTime.appendChild(tdAT_actual);
                 trAverageTime.appendChild(tdAT_goal)
                 trAverageTime.appendChild(tdAT_trend);
-                trAverageTime.appendChild(tdAT_goalDeviation);
-
-
-                //VOC
-
-
-
-                //Number of answered "YES"
-
-
-
+                trAverageTime.appendChild(tdAT_status);
 
                 //Append rows to body
                 tbody.appendChild(trBounceRate);
@@ -289,21 +223,18 @@ $(document).ready(function () {
                 accordionTable.appendChild(tbody);
                 accordionTableDiv.appendChild(accordionTable);
 
-
                 //Append header and table to accordion
                 $("#accordion").append(accordionH4);
                 $("#accordion").append(accordionTableDiv);
 
 
 
+
+
+
                 /*
-                    <tr>
-                        <th scope="row">Gjennomsnittstid</th>
-                        <td>20,42%</td>
-                        <td>30%</td>
-                        <td></td>
-                        <td>5%</td>
-                    </tr>
+                 //VOC
+                 //Number of answered "YES"
                     <tr>
                         <th scope="row">VOC</th>
                         <td>20,42%</td>
@@ -339,12 +270,119 @@ $(document).ready(function () {
         $("#uniquePageviews").text("heihei");
     });
 
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+    function calculateStatusBounceRate(actualPercentage){
+        var status;
+        if(actualPercentage < 25){
+            status = 0;
+        }
+        else if(actualPercentage >= 25 && actualPercentage < 30){
+            status = 1;
+        }
+        else if(actualPercentage >= 30){
+            status = 2;
+        }
+        else{
+            return false;
+        }
+        return status;
+    }
+
+
+
+    function calculateStatusPageviews(actualPageviews){
+        var status;
+        if(actualPageviews < 1.3){
+            status = 0;
+        }
+        else if(actualPageviews >= 1.3 && actualPageviews < 1.5){
+            status = 1;
+        }
+        else if(actualPageviews >= 1.5){
+            status = 2;
+        }
+        else{
+            return false;
+        }
+        return status;
+    }
+
+    function calculateStatusAverageTime(actualAT, goalMinAT, goalMaxAT){
+        var status;
+        if((actualAT > goalMinAT && actualAT < goalMaxAT) || actualAT == goalMinAT || actualAT == goalMaxAT){
+            status = 0;
+        }
+        else if((actualAT > goalMinAT-10 && actualAT < goalMinAT) || (actualAT > goalMaxAT && actualAT < goalMaxAT+10)){
+            status = 1;
+        }
+        else if(actualAT <= goalMinAT-10 || actualAT >= goalMaxAT){
+            status = 2;
+        }
+        else{
+            return false;
+        }
+        return status;
+    }
+
+
+
+
+
+//Temporary function to work around 'no Access-Control-Allow-Origin' from ajax calls
+    window.onload = function urlWordCounter() {
+        // jquery.xdomainajax.js  ------ from padolsey
+        jQuery.ajax = (function (_ajax) {
+            var protocol = location.protocol,
+                hostname = location.hostname,
+                exRegex = RegExp(protocol + '//' + hostname),
+                YQL = 'http' + (/^https/.test(protocol) ? 's' : '') + '://query.yahooapis.com/v1/public/yql?callback=?',
+                query = 'select * from html where url="{URL}" and xpath="*"';
+            function isExternal(url) {
+                return !exRegex.test(url) && /:\/\//.test(url);
+            }
+            return function (o) {
+                var url = o.url;
+                if (/get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url)) {
+                    // Manipulate options so that JSONP-x request is made to YQL
+                    o.url = YQL;
+                    o.dataType = 'json';
+                    o.data = {
+                        q: query.replace(
+                            '{URL}',
+                            url + (o.data ?
+                            (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
+                                : '')),
+                        format: 'xml'
+                    };
+                    // Since it's a JSONP request
+                    // complete === success
+                    if (!o.success && o.complete) {
+                        o.success = o.complete;
+                        delete o.complete;
+                    }
+                    o.success = (function (_success) {
+                        return function (data) {
+                            if (_success) {
+                                // Fake XHR callback.
+                                _success.call(this, {
+                                    responseText: data.results[0]
+                                    // YQL screws with <script>s
+                                    // Get rid of them
+                                        .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                                }, 'success');
+                            }};
+                    })(o.success);
+                }
+                return _ajax.apply(this, arguments);
+            };
+        })(jQuery.ajax);
+    }
+
 
 
 });
