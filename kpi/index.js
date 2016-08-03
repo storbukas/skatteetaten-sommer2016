57edbba1233731108df79f7b332f0ff85b1afa9f
Daphne Leebeek <daphneleebeek@gmail.com>
2016-08-03 15:48:22 +0200

    edited menu

--- kpi/index.js
+++ kpi/index.js
@@ -2,12 +2,7 @@
  * Created by Administrator on 30.06.2016.
  */
 
-//Current URL
-var current_page_URL = location.href;
-
-//Current path in hierarchy
-var current_sideMenu_path  = current_page_URL.split("skatteetaten.no/")[1];
-var current_subSideMenu_path  = current_page_URL.split("person/")[1];
+var active_sideMenu_url = location.href.split("/kpi")[1];
 
 //Side menu link & sub side menu link/Selectors of <a>
 var menulink = $('.sidemenu-link');
@@ -25,23 +20,15 @@ var end_dateJSONformat = moment().format('MM.DD.YY');
 
 
 $(document).ready(function () {
+
     //Changes active class on buttons in side menu and sub side menu
-    $(function changeActive() {
-        menulink.parents('li, ul').removeClass('active');
-        submenulink.parents('li, ul').removeClass('active');
-        menulink.each(function () {
-            if ($(this).attr("href") !== "#" && $(this).attr("href").split("skatteetaten.no/")[1].substr(0, 4) === current_sideMenu_path.substr(0, 4)) {
-                $(this).parent('li').addClass('active');
-                $('div.subHeaderDate h2.subHeader').text($(this).text().toUpperCase());
-            }
-        });
-        submenulink.each(function () {
-            if ($(this).attr("href") !== "#" && $(this).attr("href").split("person/")[1].substr(0, 4) === current_subSideMenu_path.substr(0, 4)) {
-                $(this).parent('li').addClass('active');
-                $(this).children('.sidebar-dot').css("border", "2px solid #000");
-            }
-        });
-    });
+    if(active_sideMenu_url == "/"){
+        $(".menu-link-side").eq(0).addClass('activeSideMenu');
+    }
+    else if(active_top_url == "/person.html"){
+        $(".menu-link-side").eq(1).addClass('activeSideMenu');
+    }
+
 
     //DatePicker
     var datePicker = $("#datepickerInput");
